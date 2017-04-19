c9e2f47df69c8b06f4fb9ce63616ca777484968e
Donald Stolz <donaldtstolz@gmail.com>
2017-04-19 13:25:32 -0500

    Update AddClassGUI.java

--- src/ScheduleMaker/AddClassGUI.java
+++ src/ScheduleMaker/AddClassGUI.java
@@ -3,7 +3,13 @@
 		-Called from main class 'Schedule'
 		-Collects information to store in 'ScheduleEvent'
 		-Returns newly 'ScheduledEvent' to 'Schedule'
-
+	Notes:
+		Will need to extend class JFrame JFrame
 	TO_DO
-		-
-*/
+		Declare variables (courseName, day(s), startTime, endTime, location, anything else? ) to collect
+		
+		create JTextFields, OptionPanes, or other form to collect data in proper format
+		
+		create a new 'ScheduleEvent' with variables
+		return new 'ScheduleEvent' to main class
+ */
