9a1a5b421fc43ff2ce8f05d8b3daab90ef7d1f3c
Donald Stolz <donaldtstolz@gmail.com>
2017-04-20 12:58:24 -0500

    Update and rename BuildSchedule.java to LoyolaSchedule.java

---
+++ src/loyolaScheduleBuilder/LoyolaSchedule.java
@@ -0,0 +1,26 @@
+// GUI (Swing) class for displaying schedule
+// Contains main method
+/*
+	BASIC_STRUCTURE
+		-JButton AddClass
+		-JButton RemoveClass
+		-JButton Print
+		-Jbutton NewSchedule
+		-JLabel[42] TimeSlots
+		-Keylistener for buttons
+		
+	TO_DO
+		- Create constructor
+		- Create event listeners for jButtons
+		- Create methods for calling other classes
+*/
+package loyolaScheduleBuilder;
+
+import java.util.*;
+
+public class LoyolaSchedule
+{
+	public static void main(String[] args)
+	{
+		
+	}
