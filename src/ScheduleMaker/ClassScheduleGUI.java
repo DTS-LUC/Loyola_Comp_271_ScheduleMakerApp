ff9f24e329b26fdf244b4a1806fdc60ec71d5596
Donald Stolz <donaldtstolz@gmail.com>
2017-04-20 12:14:55 -0500

    Update ClassScheduleGUI.java

--- src/ScheduleMaker/ClassScheduleGUI.java
+++ src/ScheduleMaker/ClassScheduleGUI.java
@@ -1,4 +1,14 @@
 // GUI (Swing) class for displaying schedule
 /*
+	BASIC_STRUCTURE
+		-JButton AddClass
+		-JButton RemoveClass
+		-JButton Print
+		-Jbutton NewSchedule
+		-Keylistener for buttons
+		
+		
+		
+		
 	TO_DO
 */
