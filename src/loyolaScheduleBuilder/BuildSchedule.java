e0cebd0f36c4f1e0c3b6f03cf1175421dcbf2902
Donald Stolz <donaldtstolz@gmail.com>
2017-04-20 12:40:11 -0500

    Create BuildSchedule.java

---
+++ src/loyolaScheduleBuilder/BuildSchedule.java
@@ -0,0 +1,17 @@
+// GUI (Swing) class for displaying schedule
+// Contains main method
+/*
+	BASIC_STRUCTURE
+		-JButton AddClass
+		-JButton RemoveClass
+		-JButton Print
+		-Jbutton NewSchedule
+		-Keylistener for buttons
+		
+		
+		
+		
+	TO_DO
+*/
+package ScheduleMaker
+publice static void
