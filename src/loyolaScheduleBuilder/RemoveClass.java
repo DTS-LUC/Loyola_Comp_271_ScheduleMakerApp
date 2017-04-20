5de79f7fd95a76a672f850b2c5dac5eefa2ea989
Donald Stolz <donaldtstolz@gmail.com>
2017-04-20 13:02:09 -0500

    Create RemoveClass.java

---
+++ src/loyolaScheduleBuilder/RemoveClass.java
@@ -0,0 +1,9 @@
+/* Remove Class
+	Similar to Add class
+	
+		BASIC_STRUCTURE
+			- Combo box that displays current classes
+			- Returns location of box selected
+			- The main class will then clear the time slot label
+			
+*/
