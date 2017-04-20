469f11180b37ae7a7dbcb130828805ea464a4e60
Donald Stolz <donaldtstolz@gmail.com>
2017-04-20 13:27:47 -0500

    Update addClass.java

--- src/loyolaScheduleBuilder/addClass.java
+++ src/loyolaScheduleBuilder/addClass.java
@@ -3,15 +3,20 @@
 		-Called from main class 'Schedule'
 		-Collects information to store in 'ScheduleEvent'
 		-Returns newly 'ScheduledEvent' to 'Schedule'
+		
+	BASIC_STRUCTURE
+	
+	
 	Notes:
 		Will need to extend class JFrame JFrame
 		
 		Time variable should be selected from combo box. This will make it easier to assign the proper jlabel for time slots
 			-Will also limit to classes that don't align perfectly - Difficult for lab times
 	TO_DO
-		Declare variables (courseName, day(s), time, location, anything else? ) to collect
+		Declare variables (courseName, day(s), time, location, bgColor, anything else? ) to collect
 		
 		create JTextFields, OptionPanes, or other form to collect data in proper format
+		- JColorChooser to select background
 		
 		create a new 'ScheduleEvent' with variables
 		return new 'ScheduleEvent' to main class
