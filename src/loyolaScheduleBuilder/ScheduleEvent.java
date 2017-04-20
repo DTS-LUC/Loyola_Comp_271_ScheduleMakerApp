80cca7b9e526a1729639909c62a5cdfd81705daf
Donald Stolz <donaldtstolz@gmail.com>
2017-04-20 18:12:00 -0500

    Update ScheduleEvent.java

--- src/loyolaScheduleBuilder/ScheduleEvent.java
+++ src/loyolaScheduleBuilder/ScheduleEvent.java
@@ -1,1 +1,10 @@
 
+package loyolaScheduleBuilder;
+
+public class ScheduleEvent
+{
+	private char[4] department;
+	private int	courseNumber;
+	private String courseName;
+	private int	day;	// Inte
+	private int time;
