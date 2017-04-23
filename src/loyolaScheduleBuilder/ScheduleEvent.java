f9229aca7129f17e700209d15e3c4a091ce531e5
Donald Stolz <donald@Harrisons-MacBook-Pro.local>
2017-04-23 13:10:07 -0500

    Updated schedule event class

--- src/loyolaScheduleBuilder/ScheduleEvent.java
+++ src/loyolaScheduleBuilder/ScheduleEvent.java
@@ -1,10 +1,62 @@
 
 package loyolaScheduleBuilder;
 
+import java.awt.Color;
+import java.lang.*;
+
 public class ScheduleEvent
 {
-	private char[4] department;
+	private String department;
 	private int	courseNumber;
 	private String courseName;
-	private int	day;	// Inte
-	private int time;
+	private String professor;
+	private int	daySlot;	// Integer 0 = Monday, 1 = Tuesday,...
+	private int timeSlot;
+	private Color background = Color.GREEN;
+
+	public scheduleEvent (String dept, int cNum, String cName, String prof, int dSlot, int tSlot, Color bGround)
+	{
+		// Add department
+		this.department[i] = new String(dept);
+
+		// Add course number
+
+
+		// Add course name
+
+
+		// Add professor
+
+
+		// Add day
+
+
+		// Add time
+
+
+		// Add background color
+	}
+
+	private 
+
+	private void editDepartmentNumber(String dept)
+	{
+		this.department = new String(dept);
+	}
+
+	private void editCourseNumber(int cNum)
+	{
+		this.courseNumber = cNum;
+	}
+
+	private void editCourseName(String cName)
+
+	private void editProfessor(String prof)
+
+	private void editDay(int dSlot)
+
+	private void editTime(int tSlot)
+
+	private void editBackground(Color bGround)
+
+}
