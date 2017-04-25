c417afbe76d8abc00b08e64f21133c4f9274b990
Donald Stolz <donald@Harrisons-MacBook-Pro.local>
2017-04-25 17:02:22 -0500

    Working on controller class and main view frame

--- src/loyolaScheduleBuilder/LoyolaSchedule.java
+++ src/loyolaScheduleBuilder/LoyolaSchedule.java
@@ -1,14 +1,13 @@
 // GUI (Swing) class for displaying schedule
-// Contains main method
 /*
-	BASIC_STRUCTURE
+	BASIC_STRUCTURE (old)
 		-JButton AddClass
 		-JButton RemoveClass
 		-JMenu {"New", "Print"}
 		-JLabel[42] TimeSlots (Will just read, then write, data recieved from 'LoyolaEvent'
 		-Keylistener for buttons
 		
-	TO_DO
+	TO_DO (old)
 		- Create constructor
 		- Create event listeners for jButtons
 		- Create methods for calling other classes
@@ -28,10 +27,39 @@ public class LoyolaSchedule extends JFrame
 {
 	private JButton addButton = new JButton("Add Class");
 	private JButoon removeButton = new JButton("Remove Class");
-	//private JLabel scheduleSlot = new JLabel
+	// ?? Private JLabel scheduleSlot = new JLabel??
+	// Eventually a print button
 	
-	LoyolaSchedule()
+	public LoyolaSchedule()
 	{
-		
+		this.setSize(800, 800);
+
+		Toolkit tk = Toolkit
+
+		this.setVisible(true);
+
+		Dimension dim = tk.getScreenSize();
+
+		// Find the center of the screen
+		int xPos = (dim.width / 2) - (this.getWidth() / 2);
+		int yPos = (dim.height / 2) - (this.getHeight() / 2);
+
+		this.setLocation(xPos, yPos);
+
+		this.setResizable(false);
+
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		this.setTitle("Loyola Schedule Builder");
+
+		/* TO_DO:
+			Set Layout
+			Create Add Button
+				- Create ActionListener
+			Create Remove Button
+				- Create ActionListener
+
+
+		*/
 	}
 }
