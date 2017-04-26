72688845cad33399e209f74f55b49ee6cd6d4080
Donald Stolz <donald@Harrisons-MacBook-Pro.local>
2017-04-26 13:56:50 -0500

    Finished ScheduleEvent Object

--- src/loyolaScheduleBuilder/ScheduleEvent.java
+++ src/loyolaScheduleBuilder/ScheduleEvent.java
@@ -10,31 +10,32 @@ public class ScheduleEvent
 	private int	courseNumber;
 	private String courseName;
 	private String professor;
-	private int	daySlot;	// Integer 0 = Monday, 1 = Tuesday,...
+	private int[]	daySlots;	// Integer 0 = Monday, 1 = Tuesday,...
 	private int timeSlot;
 	private Color background = Color.GREEN;
 
-	public scheduleEvent (String dept, int cNum, String cName, String prof, int dSlot, int tSlot, Color bGround)
+	public ScheduleEvent (String dept, int cNum, String cName, String prof, int[] dSlot, int tSlot, Color bGround)
 	{
 		// Add department
-		this.department[i] = new String(dept);
+		this.department = new String(dept);
 
 		// Add course number
-
+		this.courseNumber = cNum;
 
 		// Add course name
-		
+		this.courseName = cName;
 
 		// Add professor
-
+		this.professor = new String(prof);
 
 		// Add day
-
+		this.daySlots = dSlot;
 
 		// Add time
-
+		this.timeSlot = tSlot;
 
 		// Add background color
+		this.background = bGround;
 	} 
 
 	private void setDepartmentNumber(String dept)
@@ -49,31 +50,51 @@ public class ScheduleEvent
 
 	public void setCourseName(String cName)
 	{
-
+		this.courseName = cName;
 	}
 
 	public void setProfessor(String prof)
 	{
-		
+		this.professor = new String(prof);		
 	}
 
-	public void setDay(int dSlot)
+	public void setDays(int[] dSlot)
 	{
-		
+		this.daySlots = dSlot;
 	}
 
 	public void setTime(int tSlot)
 	{
-		
+		this.timeSlot = tSlot;
 	}
 
 	public void setBackground(Color bGround)
 	{
-		
+		this.background = bGround;
 	}
 		
 	public String getDeptartment()
 	{
 		return department;
+	}
+
+	public int getCourseNumber()
+	{
+		return this.courseNumber;
+	}
+
+	public String getCourseName()
+	{
+		return this.courseName;
+	}
+
+	public String getProfessor()
+	{
+		return this.professor;
+	}
+
+	public int[] getDays()
+	{
+		return this.daySlots;
 	}
 }
