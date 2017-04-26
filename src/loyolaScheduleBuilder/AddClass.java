3f2ec12224856b2d0f57de532216f7421c4d89b3
nbonifas <nbonifas@users.noreply.github.com>
2017-04-26 14:48:15 -0500

    Update AddClass.java

--- src/loyolaScheduleBuilder/AddClass.java
+++ src/loyolaScheduleBuilder/AddClass.java
@@ -40,229 +40,146 @@
 		return new 'ScheduleEvent' to main class
  */
 
-	import java.awt.Button;
-	import java.awt.Dimension;
-	import java.awt.Toolkit;
-	import javax.swing.*;
-	
-	import java.awt.event.*;
-	
-	import java.util.Scanner;
+/*Add Class 
+		BASIC_STRUCTURE
+			- Add Class Button with textArea for user input of class
+			- Day Check Boxes (M-F)
+			- Time: Start and End --> Drop down boxes for 01-12 hour, 00-59 min, am & pm buttons
+			- Location: LSC only... Building Drop down, room number user input
+					For program additions later on, we could also put
+						-Campus drop down
+						-Professor (User input)
+							--with link to info/pic of prof
+							--link to syllabus & office hour schedule
+								---option to display office hour schedule over your own to see when you could go
+						-Subject & Class #
+						-etc. ... time constraints though so we will not work with these right now
+			- Color of class input
+			- The main class will then add the class input
+			
+*/
 
-	//Extends JFrame so it can create frames
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import javax.swing.*;
+
+import java.awt.event.*;
+
+public class AddCourse extends JFrame{
 	
-public class AddClass extends JFrame{
+	JButton addCourseB;
+	JTextField addCourseTF;
+	JTextArea addCourseTxtA;
+	String newCourse; 
 	
-	JButton button1;
-	JTextField textField1;
-	JTextArea textArea1;
-	String newClass= " ";
-	
-	public static void main (String[] args){
+	public static void main(String[] args){
 		
-		new AddClass(); //Calls constructor file that creates user interface for us
+		new AddCourse();
+		
 	}
-	
-	public AddClass(){ //AddClass is the constructor file itself
+
+	public AddCourse(){
 		
-		this.setSize(600,600); //sets the size for the window/frame that appears on the screen (in Java it's a frame, in everything else it's called a window)
+		//Panel: newCourseP ... houses components of Course Addition
 		
-		Toolkit tk = Toolkit.getDefaultToolkit(); //Tool Kit object that lets you get width and height of the screen
+		JPanel newCourseP = new JPanel();
+		newCourseP.setLayout(new BorderLayout());
 		
-		Dimension dim = tk.getScreenSize(); //allows you to store that height and width
-		
-		int xPos = (dim.width/2) - (this.getWidth()/2); 
-		//Allows you to figure out the x position; 
-		//upper left hand corner for the frame 
-		//(done by taking the dimensions of the frame, 
-		//which is the width of the screen divided by two, 
-		//and then taking the width of the actual frame or window and dividing it by two)
-		//we do the same thing for the y position
-		int yPos = (dim.height/2) - (this.getHeight()/2);
-		
-		this.setLocation(xPos,yPos); //defines what x and y position we are creating for our frame
-		
-		this.setResizable(false);
-		
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //everything should close down when the user hits the close button
-		
-		this.setTitle("Class Addition"); //Title for the frame 
-		
-		JPanel thePanel = new JPanel(); //Creates a Panel that will be inside the frame... holds all our components
-		
-		/*Panel
-		 * 
-		 * The Panel is where we will want to keep all our buttons for class details, including
-		 * day (M-F check buttons)
-		 * time (Start and end)
-		 * place (LSC only for now, but want to expand to include campus options later)... Building options, followed by a manual classroom # input
-		 * 
-		 */
-		
-		button1 = new JButton("Add Class"); //creating the button... defining what the button text will be
-		
-		//Make sure to write an exception that the class cannot be added if there is no title for it and if there is no Day/time picked
-		
-		ListenForButton addClassButton = new ListenForButton();
-		
-		button1.addActionListener(addClassButton);
-		
-		thePanel.add(button1);
-		
-		//The area where the New Class will be typed
-		JTextField textField1 = new JTextField("Type Class Here", 35);
-		
-		ListenForKeys lForKeys = new ListenForKeys();
-		
-		textField1.addKeyListener(lForKeys);
-		
-		thePanel.add(textField1);
-		
-		//Text Area: Where the classes added will be displayed
-		JTextArea textArea1 = new JTextArea(15,20);
-		
-		textArea1.setText("Classes to Be Added:\n");
-		
-		textArea1.setLineWrap(true); //Whenever we get to text area end we want it to wrap down to the next line
-		
-		textArea1.setWrapStyleWord(true); //Don't split words
-		
-		JScrollPane scrollbar1 = new JScrollPane(textArea1, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED); //defines scrollbars as needed
-		
-		thePanel.add(scrollbar1);
-		
-		this.add(thePanel);
-		
-		ListenForWindow lForWindow = new ListenForWindow();
-		
-		this.addWindowListener(lForWindow);
+			//Button for newCourseP
+			addCourseB = new JButton("Add Course");
+			
+			newCourseP.add(addCourseB); //adds it to the panel
+			
+				//Text Field: newCoursePTF... where user will input their new Course
+				JTextField newCoursePTF = new JTextField("", 35);
+				
+				newCoursePTF.setColumns(10);
+				
+				newCoursePTF.setText("");
+				
+				newCourseP.add(newCoursePTF); //adds field to panel
+			
+			//JCheckbox --> Days
+			
+			JPanel checkBoxPanel = new JPanel();
+				
+			JCheckBox mButton;
+			JCheckBox tButton;
+			JCheckBox wButton;
+			JCheckBox thButton;
+			JCheckBox fButton; 
+			
+			/*
+			 * There are 25 possible combinations for these days... 
+			 * 0 days
+			 * 1 of the 5 days (5! ways, 5 without difference )
+			 * 2 of the five days (60 [5!/2!], ...)
+			 * 3 of the five days (20)
+			 * 4 of the five days (5)
+			 * 5 days
+			 */
+			
+				//Here we create the checkboxes and add them to the check box panel
+				mButton = new JCheckBox("M");
+				checkBoxPanel.add(mButton);
+				
+				tButton = new JCheckBox("T");
+				checkBoxPanel.add(tButton);
+				
+				wButton = new JCheckBox("W");
+				checkBoxPanel.add(wButton);
+				
+				thButton = new JCheckBox("TH");
+				checkBoxPanel.add(thButton);
+				
+				fButton = new JCheckBox("F");
+				checkBoxPanel.add(fButton);
+				
+				newCourseP.add(checkBoxPanel);
+				
+				
+				
+				
+				//Now the Listeners
+				mButton.addDayListener(this);
+				tButton.addDayListener(this);
+				wButton.addDayListener(this);
+				thButton.addDayListener(this);
+				fButton.addDayListener(this);
+				
+				//Indicate what days you can choose
+				choices = new StringBuffer("M-T-W-TH-F");
+				
+				
+				
+				add(addCourseP, BorderLayout.LINE_START);
+				setBorder(BorderFactory.createEmptyBorder(20,20,20,20));// Unclear from java tutorial on oracle??? 
+				
+				
+			//JComboBox & JRadioButton--> Time
+			private
+			
+			}
+			
+				
+				
+			
+			
+			
+			
+		this.add(newCourseP);
 		
 		this.setVisible(true);
 		
-		ListenForMouse lForMouse = new ListenForMouse();
-		thePanel.addMouseListener(lForMouse);
+	}
+	
+	//Implements Listeners for all our controls (i.e., gives them actions)
+	
+	private class dayCheckBox implements ItemListener{
+		
+		public void actionPerformed(KeyEvent )
 		
 	}
 	
-	//Implement Listeners
-	
-	private class ListenForButton implements ActionListener{
-		
-		public void actionPerformed(ActionEvent e){
-			
-			if (e.getSource() == button1){
-				
-				textField1
-				
-				textArea1.append("Class Added: " + newClass);
-				
-				
-			}
-		}
-	}
-	
-	private class ListenForKeys implements KeyListener{
-
-		
-		public void keyPressed(KeyEvent e) {
-			textArea1.append("Key Hit: " + e.getKeyChar() + "\n");
-			
-		}
-
-		@Override
-		public void keyReleased(KeyEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void keyTyped(KeyEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
-			
-	}
-	
-	private class ListenForWindow implements WindowListener{
-
-		@Override
-		public void windowActivated(WindowEvent e) { //called whenever the window is an active window
-			textArea1.append("Window is active");
-			
-		}
-
-		//this.dispose is called (this.dispose();)
-		public void windowClosed(WindowEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void windowClosing(WindowEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void windowDeactivated(WindowEvent e) {//called if window is no longer the active window... maybe clicking on some other window
-			textArea1.append("Window is Not active");
-			
-		}
-
-		@Override
-		public void windowDeiconified(WindowEvent e) {
-			textArea1.append("Window is in Normal State");
-			
-		}
-
-		@Override
-		public void windowIconified(WindowEvent e) {
-			textArea1.append("Window is minimized");
-			
-		}
-
-		@Override
-		public void windowOpened(WindowEvent e) {
-			textArea1.append("Window is Created");
-			
-		}
-		
-	}
-	
-	private class ListenForMouse implements MouseListener{
-
-		@Override
-		public void mouseClicked(MouseEvent e) {
-			textArea1.append("Mouse Panel pos: " + e.getX() + " " + e.getY()+"\n");
-			textArea1.append("Mouse Screen pos: " + e.getXOnScreen() + " " + e.getYOnScreen() + "\n");
-			textArea1.append("Mouse Button: " + e.getButton() + "\n");
-			textArea1.append("Mouse Clicks: " + e.getClickCount() + "\n");
-		
-		}
-
-		@Override
-		public void mouseEntered(MouseEvent arg0) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void mouseExited(MouseEvent arg0) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void mousePressed(MouseEvent arg0) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void mouseReleased(MouseEvent arg0) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-	}
 }
